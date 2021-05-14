6a119117df863d3d7e86c8a44a0513ca0ea83e5d
ppanigrahisau <82024760+ppanigrahisau@users.noreply.github.com>
2021-05-14 15:10:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+//var mouse,mouseImg,cat,catImg;
+var bgImg;
+function preload() {
+    //load the images here
+    //catImg=loadImage("cat1.png|");
+    bgImg=loadImage("gerden.png");
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+    
+}
+
+function draw() {
+
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
